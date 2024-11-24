e5bb06975f1b4370908317b444e3b9cdf46feba3
ogreverse <glado.o.guild@gmail.com>
2024-11-24 10:42:44 +0900

    update: format files

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -1,9 +1,9 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
-    './renderer/pages/**/*.{js,ts,jsx,tsx}',
-    './renderer/components/**/*.{js,ts,jsx,tsx}',
+    "./renderer/pages/**/*.{js,ts,jsx,tsx}",
+    "./renderer/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     colors: {
@@ -15,4 +15,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
